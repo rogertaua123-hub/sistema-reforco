5e7cc1ebbec53eeb3057b1a4de1685ac4677d017
rogertaua123-hub <rogertaua123@gmail.com>
2026-03-06 16:39:19 -0300

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,39 +1,3 @@
-import { auth, db } from "./firebase.js";
-
-import { signInWithEmailAndPassword }
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-import { doc, getDoc }
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-window.login = async function(){
-
-let email = document.getElementById("email").value;
-let senha = document.getElementById("senha").value;
-
-let cred = await signInWithEmailAndPassword(auth,email,senha);
-
-let uid = cred.user.uid;
-
-let docRef = doc(db,"usuarios",uid);
-
-let usuario = await getDoc(docRef);
-
-let tipo = usuario.data().tipo;
-
-if(tipo == "admin"){
-
-window.location = "dashboard.html";
-
-}
-
-else{
-
-window.location = "pais.html";
-
-}
-
-}
 import { auth } from "./firebase.js";
 
 import { signInWithEmailAndPassword }
@@ -47,6 +11,8 @@ let senha = document.getElementById("senha").value;
 signInWithEmailAndPassword(auth,email,senha)
 
 .then(()=>{
+
+alert("Login realizado");
 
 window.location = "dashboard.html";
 
