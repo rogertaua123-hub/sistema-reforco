70a2d02acd3d08d9f0b8493a6c3dfcc1bbe0ad22
rogertaua123-hub <rogertaua123@gmail.com>
2026-03-06 16:29:39 -0300

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,18 +1,36 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
+
 import { signInWithEmailAndPassword }
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-window.login = function(){
+import { doc, getDoc }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+window.login = async function(){
 
 let email = document.getElementById("email").value;
 let senha = document.getElementById("senha").value;
 
-signInWithEmailAndPassword(auth,email,senha)
-.then(()=>{
+let cred = await signInWithEmailAndPassword(auth,email,senha);
+
+let uid = cred.user.uid;
+
+let docRef = doc(db,"usuarios",uid);
+
+let usuario = await getDoc(docRef);
+
+let tipo = usuario.data().tipo;
+
+if(tipo == "admin"){
+
 window.location = "dashboard.html";
-})
-.catch(()=>{
-alert("Login inválido");
-});
+
+}
+
+else{
+
+window.location = "pais.html";
+
+}
 
 }
