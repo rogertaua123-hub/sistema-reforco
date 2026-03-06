fe57ac4c5308e6c911c2c942d5c710b9d9da2ec8
rogertaua123-hub <rogertaua123@gmail.com>
2026-03-06 16:40:27 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,17 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID"
+
+apiKey: "SUA_API_KEY",
+
+authDomain: "SEU_AUTH_DOMAIN",
+
+projectId: "SEU_PROJECT_ID",
+
+storageBucket: "SEU_BUCKET",
+
+messagingSenderId: "SEU_ID",
+
+appId: "SEU_APP_ID"
+
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
 export const auth = getAuth(app);
