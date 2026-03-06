ffdc4972bb8dced56c065a9d4724a289aeb7570f
rogertaua123-hub <rogertaua123@gmail.com>
2026-03-06 16:33:35 -0300

    Add files via upload

--- auth.js
+++ auth.js
@@ -34,3 +34,28 @@ window.location = "pais.html";
 }
 
 }
+import { auth } from "./firebase.js";
+
+import { signInWithEmailAndPassword }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+window.login = function(){
+
+let email = document.getElementById("email").value;
+let senha = document.getElementById("senha").value;
+
+signInWithEmailAndPassword(auth,email,senha)
+
+.then(()=>{
+
+window.location = "dashboard.html";
+
+})
+
+.catch((erro)=>{
+
+alert("Erro: " + erro.message);
+
+});
+
+}
